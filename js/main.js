f4406f370bf7613eb0bb2e5d0c5f623f8fb7b02b
StepanIonov <stiepan.ionov@mail.ru>
2022-06-24 17:14:13 +0300

    Finish main part

--- js/main.js
+++ js/main.js
@@ -6,4 +6,32 @@ mobileNavButton.addEventListener("click", () => {
     mobileNavIcon.classList.toggle("active");
     mobileNav.classList.toggle("active");
     document.body.classList.toggle("no-scroll")
-})
+});
+
+const videoBtn = document.querySelector("#video-story-btn");
+const videoBtnIcon = document.querySelector("#video-story-btn-icon");
+const videoOverlay = document.querySelector("#video-story-overlay");
+const videoFile = document.querySelector("#video-story");
+
+
+videoBtn.addEventListener("click", () => {
+    function toggleOverlay(event) {
+        if (event.type === "mouseleave") {
+            videoOverlay.classList.add("hidden");
+        } else {
+            videoOverlay.classList.remove("hidden");
+        }
+    }
+
+    if (videoFile.paused) {
+        videoFile.play();
+        videoBtnIcon.src = "./img/story/pause-white.svg";
+        videoOverlay.onmouseleave = toggleOverlay; // не используется addEvent, т. к. необходимо перезаписывать слушатель, а не новый
+        videoOverlay.onmouseenter = toggleOverlay;
+    } else {
+        videoFile.pause();
+        videoBtnIcon.src = "./img/story/play-white.svg";
+        videoOverlay.onmouseleave = null;
+        videoOverlay.onmouseenter = null;
+    }
+});
